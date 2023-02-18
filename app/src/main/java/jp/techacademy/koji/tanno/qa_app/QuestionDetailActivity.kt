3c658e01537b28c5742ee67955befa2c842b0929
koji.tanno <k.mouse0820@gmail.com>
2023-02-18 14:19:18 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/koji/tanno/qa_app/QuestionDetailActivity.kt
@@ -0,0 +1,96 @@
+package jp.techacademy.koji.tanno.qa_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_question_detail.*
+
+class QuestionDetailActivity : AppCompatActivity() {
+
+    private lateinit var mQuestion: Question
+    private lateinit var mAdapter: QuestionDetailListAdapter
+    private lateinit var mAnswerRef: DatabaseReference
+
+    private val mEventListener = object : ChildEventListener {
+        override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
+            val map = dataSnapshot.value as Map<*, *>
+            val answerUid = dataSnapshot.key ?: ""
+
+            for (answer in mQuestion.answers) {
+                // 同じAnswerUidのものが存在しているときは何もしない
+                if (answerUid == answer.answerUid) {
+                    return
+                }
+            }
+
+            val body = map["body"] as? String ?: ""
+            val name = map["name"] as? String ?: ""
+            val uid = map["uid"] as? String ?: ""
+
+            val answer = Answer(body, name, uid, answerUid)
+            mQuestion.answers.add(answer)
+            mAdapter.notifyDataSetChanged()
+        }
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onChildRemoved(snapshot: DataSnapshot) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            TODO("Not yet implemented")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_detail)
+
+        // 渡ってきたQuestionのオブジェクトを保持する
+        val extras = intent.extras
+        mQuestion = extras!!.get("question") as Question
+
+        title = mQuestion.title
+
+        // ListViewの準備
+        mAdapter = QuestionDetailListAdapter(this, mQuestion)
+        listView.adapter = mAdapter
+        mAdapter.notifyDataSetChanged()
+
+        fab.setOnClickListener {
+            // ログイン済のユーザーを取得する
+            val user = FirebaseAuth.getInstance().currentUser
+
+            if (user == null) {
+                // ログインしていなければログイン画面に遷移させる
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+            } else {
+                // Questionを渡して回答作成画面を起動する
+                val intent = Intent(applicationContext, AnswerSendActivity::class.java)
+                intent.putExtra("question", mQuestion)
+                startActivity(intent)
+            }
+
+            val dataBaseReference = FirebaseDatabase.getInstance().reference
+            mAnswerRef = dataBaseReference.child(ContentsPATH).child(mQuestion.genre.toString())
+                .child(mQuestion.questionUid).child(AnswersPATH)
+            mAnswerRef.addChildEventListener(mEventListener)
+
+        }
+    }
+}
+
